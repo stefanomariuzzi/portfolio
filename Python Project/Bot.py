84f61d891b13284d2267c9f957d260a6f2d36eb9
smariuzzi <s.mariuzzi@gmail.com>
2020-08-27 11:59:19 +0200

    PEP-8 Formatting for Python Project

--- Python Project/Bot.py
+++ Python Project/Bot.py
@@ -4,12 +4,12 @@ import json
 
 
 class Bot:
-    
+
     def __init__(self):
         """Bot Initialization"""
         # dictionary initialization for data saving
         self.data = {}
-        # integer for the number of credit used. 
+        # integer for the number of credit used.
         self.credit_count = 0
         # url definition for requests
         self.url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
@@ -18,31 +18,31 @@ class Bot:
             'Accepts': 'application/json',
             'X-CMC_PRO_API_KEY': ''
         }
-        
+
     def load_key(self, file_name):
         """Load the personal key for CoinMarketCap API from a text file"""
         # return variables initialization
         key = None
-        error = 0 
+        error = 0
         try:
             # try to open the file and read the key from it. Then close it
             with open(file_name, 'r') as file:
                 key = file.read()
         except:
             # if an exception accours, catch it and save its class
-            error = 1           
+            error = 1
         # header update for requests
         self.headers = {
             'Accepts': 'application/json',
             'X-CMC_PRO_API_KEY': key
         }
         return error
-      
+
     def get_key(self):
         """Load the personal key for CoinMarketCap API from a text file"""
         # return variables initialization
         data = None
-        error = 0       
+        error = 0
         try:
             # try to open the file and read the key from it. Then close it
             with open('key.txt', 'r') as file:
@@ -52,12 +52,12 @@ class Bot:
             error = 1
             data = e.__class__
         return error, data
-    
+
     def reset_data(self):
         """Reset data stored in the class instance"""
         self.data = {}
         self.credit_count = 0
-    
+
     def save_data(self):
         """Save collected data to a json file"""
         # if there are data to save
@@ -78,7 +78,7 @@ class Bot:
             file_name = 'Nessun dato da salvare'
             error = 1
         return error, file_name
-    
+
     def load_data(self):
         """Load last data from a json file"""
         # get yesterday's file name
@@ -95,15 +95,16 @@ class Bot:
             error = 1
             data = e.__class__
         return error, data
-    
+
     def make_request(self, params):
         """API request with errors and exceptions handling"""
         # variable initialization
         error = None
-        data = None       
+        data = None
         try:
             # try to do a request to the server
-            req = requests.get(url=self.url, params=params, headers=self.headers).json()
+            req = requests.get(url=self.url, params=params,
+                               headers=self.headers).json()
         except Exception as e:
             # if an exception accours, catch it and save its class
             error = 1
@@ -119,43 +120,43 @@ class Bot:
                 data = req['status']['error_message']
             else:
                 # else save the data
-                data = req['data']                           
+                data = req['data']
         return error, data
-                
+
     def top_volume(self):
         """First answer: find the cypto with highest volume"""
         # parameter definition for the request
         params = {
-                'start': '1',
-                'limit': '1',
-                'sort_dir': 'desc',
-                'sort': 'volume_24h',
-                'convert': 'USD'
-        }  
+            'start': '1',
+            'limit': '1',
+            'sort_dir': 'desc',
+            'sort': 'volume_24h',
+            'convert': 'USD'
+        }
         # request with error handling
         error, data = self.make_request(params)
         # if there is not an error
-        if not error: 
+        if not error:
             # fetch name, symbol and volume of the crypto
             data = {
-                'name': data[0]['name'], 
-                'symbol': data[0]['symbol'], 
+                'name': data[0]['name'],
+                'symbol': data[0]['symbol'],
                 'volume': data[0]['quote']['USD']['volume_24h']
             }
             # and save it in the instance
             self.data[1] = data
         return error, data
-    
+
     def headtail_perc_change(self):
         """Second answer: find the best and worst crypto by last 24h percent change"""
         # variable initialization and parameter definition for the request
         data = {}
         params = {
-                'start': '1',
-                'limit': '100',
-                'sort': 'percent_change_24h',
-                'convert': 'USD'
-        }  
+            'start': '1',
+            'limit': '100',
+            'sort': 'percent_change_24h',
+            'convert': 'USD'
+        }
         # for cryptos in the head and tail of the list by percent change
         for field, sort_dir in zip(['head', 'tail'], ['desc', 'asc']):
             # define the proper sort direction
@@ -174,21 +175,21 @@ class Bot:
                 name = item['name']
                 symbol = item['symbol']
                 change = item['quote']['USD']['percent_change_24h']
-                data[field].append( (name, symbol, change) )
+                data[field].append((name, symbol, change))
         # save the dictionary in the instance
         self.data[2] = data
         return error, data
-    
+
     def topN_cap_price(self, N=20):
         """Third answer: find the total unit price of the first 20 crypto by capitalization"""
         # parameters definition for the request
         params = {
-                'start': '1',
-                'limit': str(N),
-                'sort_dir': 'desc',
-                'sort': 'market_cap',
-                'convert': 'USD'
-        }  
+            'start': '1',
+            'limit': str(N),
+            'sort_dir': 'desc',
+            'sort': 'market_cap',
+            'convert': 'USD'
+        }
         # request with error handling
         error, data = self.make_request(params)
         # if there is not an error
@@ -205,14 +206,14 @@ class Bot:
                 ids.append(item['id'])
             # save everything as a dictionary
             data = {
-                'total': total, 
+                'total': total,
                 'names': names,
                 'ids': ids
             }
             # and save it in the instance
             self.data[3] = data
-        return  error, data
-    
+        return error, data
+
     def topV_vol_price(self, V=76000000):
         """Forth answer: find the total unit price of all cryptos with 24h volume >= V"""
         # variables initialization
@@ -222,7 +223,7 @@ class Bot:
         limit = 100
         volume = V
         # while the volume of the crypto is above the threshold and there is not an error
-        while( (volume >= V) and (not error) ):
+        while((volume >= V) and (not error)):
             # parameters definition for the request
             params = {
                 'start': str(start),
@@ -230,7 +231,7 @@ class Bot:
                 'sort_dir': 'desc',
                 'sort': 'volume_24h',
                 'convert': 'USD'
-            }  
+            }
             # request with error handling
             error, data = self.make_request(params)
             # if there is not an error
@@ -239,9 +240,9 @@ class Bot:
                 index = 0
                 length = len(data)
                 volume = data[index]['quote']['USD']['volume_24h']
-                # while the volume of the crypto is above the threshold 
+                # while the volume of the crypto is above the threshold
                 # and the index is lower than the total lenght of the data vector
-                while( (volume >= V) and (index < length) ):
+                while((volume >= V) and (index < length)):
                     # add its price to the total and increase the index
                     total += data[index]['quote']['USD']['price']
                     index += 1
@@ -250,13 +251,13 @@ class Bot:
                     # NOTE: when index == lenght we obtain 0 from the modulo operator which is wrong
                     # however, we will not use this volume for the while condition on index
                 # data preparation for return and saving in the instance
-                data = total                            
+                data = total
                 self.data[4] = data
-                # if we have not found all the crypto with volume above the threshold, 
+                # if we have not found all the crypto with volume above the threshold,
                 # change the starting point of the request and fetch more data
                 start += limit
         return error, data
-    
+
     def check_profit(self):
         """Fifth answer: check the profit that you would have if you buyed cryptos of the Third answer"""
         # load the data from yesterday
@@ -264,7 +265,7 @@ class Bot:
         # if there is an error
         if error:
             # then return the error and its message
-            return error, old_data    
+            return error, old_data
         # else fetch the old total amount and the its of the cryptos used
         old_total = old_data["3"]['total']
         old_ids = [item for item in old_data["3"]['ids']]
@@ -274,15 +275,15 @@ class Bot:
         limit = 100
         new_total = 0
         # while we used less than 20 crypto for the evaluation and there is not an error
-        while( (count < 20) and (not error) ):
+        while((count < 20) and (not error)):
             # parameters definition for the request
             params = {
-                    'start': str(start),
-                    'limit': str(limit),
-                    'sort_dir': 'desc',
-                    'sort': 'volume_24h',
-                    'convert': 'USD'
-            }  
+                'start': str(start),
+                'limit': str(limit),
+                'sort_dir': 'desc',
+                'sort': 'volume_24h',
+                'convert': 'USD'
+            }
             # request with error handling
             error, data = self.make_request(params)
             # if there is not an error
@@ -291,7 +292,7 @@ class Bot:
                 index = 0
                 # while we used less than 20 crypto for the evaluation
                 # and the index is lower than the total lenght of the data vector
-                while( (count < 20) and (index < len(data)) ):
+                while((count < 20) and (index < len(data))):
                     # get crypto id
                     new_id = data[index]['id']
                     # if there is that id in the ids used yesterday
@@ -309,4 +310,4 @@ class Bot:
                 # data preparation for return and saving in the instance
                 data = (new_total - old_total) / old_total * 100
                 self.data[5] = data
-        return error, data        
+        return error, data
--- Python Project/main.py
+++ Python Project/main.py
@@ -4,11 +4,13 @@ import time
 from Bot import Bot
 
 # function definition for report building
+
+
 def report(bot):
     # number of dashes for prints
     dashes = 50
     # header print
-    print('\n', '-'*dashes, 'INIZIO REPORT', '-'*dashes, sep='')
+    print('\n', '-' * dashes, 'INIZIO REPORT', '-' * dashes, sep='')
     # error vector definition
     errors = []
     # reset bot data
@@ -26,20 +28,20 @@ def report(bot):
         text = '\n1. La criptovaluta con il volume maggiore delle '
         text += 'ultime 24 ore è {} ({}), con un volume di {:.2f}$'
         print(text.format(data['name'], data['symbol'], data['volume']))
-    
+
     # evaluate headtail_per_change... etc (as before)
     error, data = bot.headtail_perc_change()
     errors.append(error)
     if error:
         print('\n2. ERROR:', data)
-    else: 
+    else:
         print('\n2.1 Le migliori 10 criptovalute (per incremento in percentuale delle ultime 24 ore) sono:')
         for cry in data['head']:
             print('\t+{:.2f}% - {} ({})'.format(cry[2], cry[0], cry[1]))
         print('\n2.2 Le peggiori 10 criptovalute (per incremento in percentuale delle ultime 24 ore) sono:')
         for cry in data['tail']:
-            print('\t{:.2f}% - {} ({})'.format(cry[2], cry[0], cry[1]))   
-        
+            print('\t{:.2f}% - {} ({})'.format(cry[2], cry[0], cry[1]))
+
     error, data = bot.topN_cap_price()
     errors.append(error)
     if error:
@@ -50,18 +52,18 @@ def report(bot):
         print(text.format(data['total']), end='')
         for i in range(2):
             print('\n\t', end='')
-            for name in data['names'][i*10:(i+1)*10]:
+            for name in data['names'][i * 10:(i + 1) * 10]:
                 print(name, end=', ')
-            
-    error, data = bot.topV_vol_price()  
+
+    error, data = bot.topV_vol_price()
     errors.append(error)
     if error:
         print('\n4. ERROR:', data)
-    else:     
+    else:
         text = '\n\n4. La quantità di denaro necessaria per acquistare una unità di tutte le criptovalute'
         text += ' il cui volume delle ultime 24 ore sia superiore a 76.000.000$ è di {:.2f}$'
-        print(text.format(data))       
-    
+        print(text.format(data))
+
     error, data = bot.check_profit()
     errors.append(error)
     if error:
@@ -71,21 +73,23 @@ def report(bot):
         text += 'comprato una unità di ciascuna delle prime 20 criptovalute il giorno '
         text += 'prima (ipotizzando che la classifca non sia cambiata) è del {:.2f}%'
         print(text.format(data))
-    
+
     error, file = bot.save_data()
     errors.append(error)
     if error:
         print('\n*. ERROR:', file)
     else:
         print('\n*. Dati salvati nel file', file)
-        
+
     print('\n*. Per questo report sono stati utilizzati {} crediti.'.format(bot.credit_count))
-    print('\n', '-'*dashes, '-FINE REPORT-', '-'*dashes, sep='')
+    print('\n', '-' * dashes, '-FINE REPORT-', '-' * dashes, sep='')
     return sum(errors) > 0
 
 # function definition for conversion between datetime.time() and seconds
+
+
 def sec(t):
-    return t.hour*60**2 + t.minute*60**1 + t.second*60**0 + t.microsecond*10**-6
+    return t.hour * 60**2 + t.minute * 60**1 + t.second * 60**0 + t.microsecond * 10**-6
 
 
 if __name__ == '__main__':
@@ -100,12 +104,14 @@ if __name__ == '__main__':
             input('Caricamento chiave privata, premere INVIO per continuare: ')
             error = bot.load_key('key.txt')
             if error:
-                print('ERROR: Chiave non trovata, assicurarsi di avere il file \'key.txt\' con la chiave inserita\n')
+                print(
+                    'ERROR: Chiave non trovata, assicurarsi di avere il file \'key.txt\' con la chiave inserita\n')
             else:
                 print('SUCCESS: Chiave caricata correttamente')
         # now date evaluation and printing for reference
         now = datetime.now()
-        print('\nData: ', now, '\nNOTA: Per i prossimi inserimenti, utilizzare il formato ORA:MIN:SEC', sep='')
+        print('\nData: ', now,
+              '\nNOTA: Per i prossimi inserimenti, utilizzare il formato ORA:MIN:SEC', sep='')
         # user input of the desired report hour and its period
         valid_in = False
         while(not valid_in):
@@ -118,31 +124,33 @@ if __name__ == '__main__':
         valid_in = False
         while(not valid_in):
             try:
-                pet = input('\tB) Periodo di tempo tra due reports: ')                
+                pet = input('\tB) Periodo di tempo tra due reports: ')
                 pet = datetime.strptime(pet, '%H:%M:%S')
                 valid_in = True
             except ValueError:
                 print('\tERROR: Utilizzare il formato indicato')
-        
+
         # if chosen time is passed by 10 seconds today, set day to 1 else 0
         days = 1 if sec(now.time()) + 10 >= sec(myt.time()) else 0
         # chosen time conversion to datetime (if day is 1 the first report will be tommorw)
-        myt = '{}-{}-{} {}:{}:{}'.format(now.year, now.month, now.day + days, myt.hour, myt.minute, myt.second)   
-        myt = datetime.strptime(myt, '%Y-%m-%d %H:%M:%S') 
+        myt = '{}-{}-{} {}:{}:{}'.format(now.year, now.month,
+                                         now.day + days, myt.hour, myt.minute, myt.second)
+        myt = datetime.strptime(myt, '%Y-%m-%d %H:%M:%S')
         # reports loop
         error = 0
-        while(not error): 
+        while(not error):
             text = '\nIl prossimo scarico dati avverrà in data {}'
             text += '\nPremere CTRL + C per interrompere l\'esecuzione del programma'
             print(text.format(myt))
             # missing seconds evaluation
             seconds_missing = sec(myt.time()) - sec(datetime.now().time())
             # if tomorrow, add an entire day to it (in seconds)
-            seconds_missing = seconds_missing if seconds_missing > 0 else seconds_missing + 24*60**2
+            seconds_missing = seconds_missing if seconds_missing > 0 else seconds_missing + 24 * 60**2
             # sleep for the evaluated amount
             time.sleep(seconds_missing)
             # add chosen period to the chosen time of the day
-            myt = myt + timedelta(hours=pet.hour, minutes=pet.minute, seconds=pet.second)
+            myt = myt + timedelta(hours=pet.hour,
+                                  minutes=pet.minute, seconds=pet.second)
             # start report
             error = report(bot)
             # if an error occours
@@ -152,4 +160,3 @@ if __name__ == '__main__':
     # KeyboardInterrupt exception handling
     except KeyboardInterrupt:
         print('\n\nCTRL + C: Esecuzione interrotta')
-        
